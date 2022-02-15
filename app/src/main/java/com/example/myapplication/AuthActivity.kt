5f4c0141e9ead72a87036139b1c5f1bc1fcff691
e.gutko@pixelplex.io <e.gutko@pixelplex.io>
2022-02-15 11:11:01 +0300

    [main] - added new activity

---
+++ app/src/main/java/com/example/myapplication/AuthActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textview.MaterialTextView
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.auth_activity)
+
+        val text = findViewById<MaterialTextView>(R.id.secondText)
+
+        val textTwo = findViewById<MaterialTextView>(R.id.secondTextTwo)
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        text.setOnClickListener {
+            startActivity(intent)
+        }
+
+        textTwo.setOnClickListener {
+            startActivity(Intent(this, AuthActivity::class.java))
+            textTwo.text = "text"
+        }
+
+        intent.extras?.let {
+            text.text = it.get(MainActivity.MAIN_ACTIVITY_KEY).toString()
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.textView)
+
+        val intent = Intent(this, AuthActivity::class.java)
+        intent.putExtra(MAIN_ACTIVITY_KEY, 30)
+
+        text.setOnClickListener {
+            startActivity(intent)
+        }
+    }
+
+    companion object {
+        const val MAIN_ACTIVITY_KEY = "MAIN_ACTIVITY_KEY"
     }
 }
